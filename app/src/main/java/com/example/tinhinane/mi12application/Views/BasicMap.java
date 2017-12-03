4da7fbb1cb194ef943a799049fce2e6ac6e16006
taithamo <tinhinene.ait-hamouda@etu.utc.fr>
2017-12-03 22:47:35 +0100

    Basic map activity (modelized indoor map)

---
+++ app/src/main/java/com/example/tinhinane/mi12application/Views/BasicMap.java
@@ -0,0 +1,41 @@
+package com.example.tinhinane.mi12application.Views;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.tinhinane.mi12application.Helpers.Drawing;
+import com.example.tinhinane.mi12application.Helpers.MapHelper;
+import com.example.tinhinane.mi12application.Helpers.ScanUtils;
+import com.example.tinhinane.mi12application.Helpers.Vector;
+import com.example.tinhinane.mi12application.Models.Beacon;
+import com.example.tinhinane.mi12application.R;
+
+import java.util.ArrayList;
+
+public class BasicMap extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ArrayList<Beacon> beacons = (ArrayList<Beacon>) ScanUtils.getListBeacons();
+        Log.i("Tag check", beacons.toString());
+        Vector userPos = new Vector(0, MapHelper.scaleConvert(beacons.get(0).getDistance()),0);
+        Log.i("Tag Device", beacons.get(0).getmDeviceCode()+" "+ MapHelper.scaleConvert(beacons.get(0).getDistance()) );
+                //MapHelper.findPosition(beacons.get(0).getPos(), beacons.get(1).getPos(), new Vector(700,700,0), beacons.get(0).getDistance(), beacons.get(1).getDistance(), 2);
+//        Vector userPos = MapHelper.findPosition(
+//                beacons.get(0).getPos(),
+//                beacons.get(1).getPos(),
+//                new Vector(700,700,0),
+//                6.18,
+//                8.48,
+//                6.08);
+        Log.i("Tag user pos", userPos.toString());
+        setContentView(new Drawing(this, beacons, userPos));
+    }
+
+}
